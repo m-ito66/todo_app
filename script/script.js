7241ffd085121b4bcde9644d52f0be705fcb7d88
m-ito <m-ito@ruby-dev.jp>
2021-09-08 08:19:53 +0900

    登録、削除、編集機能を追加

---
+++ script/script.js
@@ -0,0 +1,54 @@
+const app = new Vue({
+  el: '#app',
+  data() {
+    return {
+      tasks: [],
+      newTask: '',
+      editIndex: -1
+    }
+  },
+  mounted() {
+    if (localStorage.getItem('tasks')) {
+      try {
+        this.tasks = JSON.parse(localStorage.getItem('tasks'))
+      } catch(e) {
+        localStorage.removeItem('tasks')
+      }
+    }
+  },
+  methods: {
+    addTask() {
+      if (!this.newTask) return
+
+      this.tasks.push(this.newTask)
+      this.newTask = ''
+      this.saveTasks()
+    },
+    editTask(key) {
+      this.editIndex = key
+      this.newTask = this.tasks[key]
+    },
+    removeTask(key) {
+      this.tasks.splice(key, 1)
+      this.saveTasks()
+    },
+    saveTasks() {
+      console.log(this.tasks)
+      const parsed = JSON.stringify(this.tasks)
+      localStorage.setItem('tasks', parsed)
+    },
+    updateTask() {
+      if (!this.newTask) return
+
+      this.tasks[this.editIndex] = this.newTask
+      this.newTask = ''
+      this.editIndex = -1
+      this.saveTasks()
+    }
+  },
+  computed: {
+    isEdit() {
+      return this.editIndex > -1
+    }
+  }
+})
